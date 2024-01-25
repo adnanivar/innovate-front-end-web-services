3b8f864219d77996691d741951f7fc1dd5d7a7f2
Adnan Ivarsson <139991430+adnanivar@users.noreply.github.com>
2024-01-25 17:58:17 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,32 @@
+}
+    });
+main();
+    for (let i = 0; i < 10; i++) {
+function main() {
+    const data = [];
+
+    }
+    return data;
+    data.forEach(item => {
+        console.log("Random Number: " + item);
+    const data = generateRandomData();
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+function generateRandomData() {
+function generateRandomData() {
+main();
+    }
+
+    return data;
+        data.push(Math.floor(Math.random() * 100) + 1);
+    });
+}
+    const data = [];
+        console.log("Random Number: " + item);
+}
+    const data = generateRandomData();
+    for (let i = 0; i < 10; i++) {
+
+function main() {
+    data.forEach(item => {
